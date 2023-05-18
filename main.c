48143cf199cb8f789a7319aa1d1d81f3c2ce3186
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-05-18 20:04:40 +0300

    added non interactive prompt

--- main.c
+++ main.c
@@ -27,7 +27,26 @@ void terminal_interative(void)
 	_puts("$: ");
 }
 
+/**
+ * terminal_non_interactive : non interactive mode
+*/
+void terminal_non_interactive(void)
+{
+	char *input;
+	char **line_args;
+	int check = -1;
+	while(check == -1)
+	{
+		input = read_args_stream();
+		line_args = split_to_tokens();
+		check = builtin_exec(line_args);
+		/*manage memory*/
+		free(input);
+		free(line_args);
 
-
+		if (check >= 0)
+			exit(check);
+	}
+}
 
 
