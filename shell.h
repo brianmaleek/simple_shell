9bf4e215802827f70cab3f10759c2f37e6fa929a
washucode <estherw8525@gmail.com>
2023-05-18 22:24:05 +0300

    set up shell

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*for output*/
+int _putchar(char c);
+int _puts(char *str);
+
+
+#endif
