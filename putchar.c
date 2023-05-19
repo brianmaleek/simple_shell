407cea453d4db922eda10c5640e2c6f6d0a79b33
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-05-19 16:11:23 +0300

    added builtin functions

--- putchar.c
+++ putchar.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * _putchar - ....
+ * _putchar - function to print character
  * @c:character to output
  *
  * Return: On success 1/ on error 0
@@ -12,18 +12,54 @@ int _putchar(char c)
 }
 
 /**
- * _puts - function to print string
- * @str : string to print
-*/
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string to print
+ * Return: length of string
+ */
 
 void _puts(char *str)
 {
-	int c = 0;
+	while (*str != '\0')
+		_putchar(*str++);
 
-	while (str[c])
-	{
-		_putchar(str[c]);
-		c++;
-	}
+	_putchar('\n');
 }
 
+/**
+ * _strlen - function to print length of string
+ * @str : string to print
+ * Return: length of string
+*/
+size_t _strlen(char *str)
+{
+	size_t len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * _atoi - converts a string to integer
+ * @s: string to be converted
+ * Return: returns integer value of the converted string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
+}
--- shell.h
+++ shell.h
@@ -6,13 +6,21 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define TOK_BUFSIZE 64
 #define DELIMITER " \t\r\n\a"
-/*for output*/
+
+/*helper functions*/
 int _putchar(char c);
 void _puts(char *str);
+size_t _strlen(char *str);
 
+/*builtin functions*/
+int exec_cd(char **args);
+int exec_env(char **args);
+int exec_exit(char **args);
+int exec_help(char **args);
 
 /* for  reading  arguments*/
 char *read_args_stream(void);
