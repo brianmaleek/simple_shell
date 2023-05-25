8c16c1eb467b229cb535cc083d920970f642bc6c
maleekmalik@gmail.com <maleekmalik@gmail.com>
2023-05-25 10:00:51 +0300

    Revert "betty fixes, sorted PATH" to the old commit changed codes didn't workX
    This reverts commit a4fe0f64064773ff9b02f89716637f611036ab6d.

--- read_args_stream.c
+++ read_args_stream.c
@@ -37,54 +37,19 @@ char *read_args_stream(void)
 		if (position >= bufsize - 1)
 		{
 			bufsize += bufsize;
-			input_line = reallocate_buffer(input_line, sizeof(char) * bufsize);
+			input_line = realloc(input_line, sizeof(char) * bufsize);
+			if (input_line == NULL)
+			{
+				perror("error in read_args_stream.c: realloc failed\n");
+				exit(EXIT_FAILURE);
+			}
 		}
-		if (is_end_of_input(input_line, position))
+		if (input_line[position - 1] == '\n')
 		{
-			return (finalize_input(input_line, position));
+			input_line[position - 1] = '\0';
+			return (input_line);
 		}
 	}
 	free(input_line);
 	exit(EXIT_SUCCESS);
-}
-
-/**
- * reallocate_buffer - handles reallocation of buffer
- * @buffer: pointer to current input buffer to be reallocated
- * @new_size: integer representing new size for the buffer
- * Return: new buffer
-*/
-char *reallocate_buffer(char *buffer, int new_size)
-{
-	char *new_buffer = realloc(buffer, sizeof(char) * new_size);
-
-	if (new_buffer == NULL)
-	{
-		perror("error in read_args_stream.c: realloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	return (new_buffer);
-}
-
-/**
- * is_end_of_input - checks if last character read is newline
- * @input: character array representing input buffer
- * @position: Integer indicating current position in input buffer
- * Return: position
-*/
-int is_end_of_input(const char *input, int position)
-{
-	return (input[position - 1] == '\n');
-}
-
-/**
- * finalize_input - replace newline with null byte
- * @input: character array representing input buffer
- * @position: Integer indicating current position in input buffer
- * Return: input
-*/
-char *finalize_input(char *input, int position)
-{
-	input[position - 1] = '\0';
-	return (input);
 }
--- shell.h
+++ shell.h
@@ -27,6 +27,7 @@ char *_strcpy(char *dest, const char *src);
 char *_strcat(char *dest, const char *src);
 char *_getenv(char *name);
 
+
 /*builtin functions*/
 int exec_cd(char **args);
 int exec_env(char **args);
@@ -36,16 +37,11 @@ int exec_help(char **args);
 /* for  reading  arguments*/
 char *read_args_stream(void);
 char *read_args(void);
-char *reallocate_buffer(char *buffer, int new_size);
-int is_end_of_input(const char *input, int position);
-char *finalize_input(char *input, int position);
-
 
 /*to execute*/
 int execute(char **args);
 int builtin_args_exec(char **args);
 
-/*to parse*/
 char **split_to_tokens(char *tok);
 
 #endif
